41ff031d779a7bb69123b8f54df00043d2bf3e00
Kranthi <knuthala@atrihub.io>
2021-08-15 22:13:07 -0700

    feat(TDD Tutorial): Chapter 8 of TDD Tutorial first steps of FT for layout + styling
    [ICTRAINING - 842]

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -48,21 +48,20 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Buy peacock feathers')
 
         inputbox.send_keys(Keys.ENTER)
-
+        time.sleep(1)
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
-        # Edith wonders whether the site will remember her list. Then she sees
-        # that the site has generated a unique URL for her -- there is some
-        # explanatory text to that effect.
+        # Satisfied, she goes back to sleep
 
         def test_multiple_users_can_start_lists_at_different_urls(self):
             # Edith starts a new to-do list
@@ -106,4 +105,20 @@ class NewVisitorTest(LiveServerTestCase):
             page_text = self.browser.find_element_by_tag_name('body').text
             self.assertNotIn('Buy peacock feathers', page_text)
             self.assertIn('Buy milk', page_text)
+
+        def test_layout_and_styling(self):
+            # Edith goes to the home page
+            self.browser.get(self.live_server_url)
+            self.browser.set_window_size(1024, 768)
+
+            inputbox.send_keys('testing')
+            inputbox.send_keys(Keys.ENTER)
+            self.wait_for_row_in_list_table('1: testing')
+            # She notices the input box is nicely centered
+            inputbox = self.browser.find_element_by_id('id_new_item')
+            self.assertAlmostEqual(
+                inputbox.location['x'] + inputbox.size['width'] / 2,
+                512,
+                delta=10
+            )
 
