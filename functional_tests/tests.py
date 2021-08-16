86227162e0f2154adc2de920a891917e8205d41d
Kranthi <knuthala@atrihub.io>
2021-08-15 21:26:50 -0700

    new URL + view for adding to existing lists. FT passes :-)

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,7 +2,6 @@ from django.test import LiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
-import unittest
 import time
 
 MAX_WAIT = 10
@@ -49,14 +48,13 @@ class NewVisitorTest(LiveServerTestCase):
         inputbox.send_keys('Buy peacock feathers')
 
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
+
         self.wait_for_row_in_list_table('1: Buy peacock feathers')
 
         # She is invited to enter a to-do item straight away
         inputbox = self.browser.find_element_by_id('id_new_item')
         inputbox.send_keys('Use peacock feathers to make a fly')
         inputbox.send_keys(Keys.ENTER)
-        time.sleep(1)
 
         # The page updates again, and now shows both items on her list
         self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
@@ -65,19 +63,6 @@ class NewVisitorTest(LiveServerTestCase):
         # Edith wonders whether the site will remember her list. Then she sees
         # that the site has generated a unique URL for her -- there is some
         # explanatory text to that effect.
-        self.fail('Finish the test!')
-
-        # She visits that URL - her to-do list is still there.
-        # Satisfied, she goes back to sleep
-
-        def test_can_start_a_list_for_one_user(self):
-            # Edith has heard about a cool new online to-do app. She goes
-            [...]
-            # The page updates again, and now shows both items on her list
-            self.wait_for_row_in_list_table('2: Use peacock feathers to make a fly')
-            self.wait_for_row_in_list_table('1: Buy peacock feathers')
-
-            # Satisfied, she goes back to sleep
 
         def test_multiple_users_can_start_lists_at_different_urls(self):
             # Edith starts a new to-do list
@@ -91,12 +76,10 @@ class NewVisitorTest(LiveServerTestCase):
             edith_list_url = self.browser.current_url
             self.assertRegex(edith_list_url, '/lists/.+')
 
-            self.assertRegex(edith_list_url, '/lists/.+')
-
             # Now a new user, Francis, comes along to the site.
 
-            ## We use a new browser session to make sure that no information
-            ## of Edith's is coming through from cookies etc
+            # We use a new browser session to make sure that no information
+            # of Edith's is coming through from cookies etc
             self.browser.quit()
             self.browser = webdriver.Firefox()
 
